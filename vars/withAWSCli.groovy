2f5cd2343307703c588d8bcd96611bd04efc5a08
sunfriendli <pengyang_fly@163.com>
2018-08-14 15:31:56 +0800

    commit.sh

--- vars/withAWSCli.groovy
+++ vars/withAWSCli.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(String cmd, String result, Closure body) {
+def call(String name, Closure body) {
     //sh 'pip3 install awscli --user'
     //sh 'export PATH=~/.local/bin:$PATH;cmd'
     //sourceAmiId = sh(script: "${findLatestAmi}", returnStdout: true).trim()
+    println(name)
+    echo "awscli groovy"
     println(this.region)
     this.sourceAmiId = "test"
 }
