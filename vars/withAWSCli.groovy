2ce5e6883ef355d29ab01a3d047be7c4f38b41df
sunfriendli <pengyang_fly@163.com>
2018-08-15 10:56:15 +0800

    commit.sh

--- vars/withAWSCli.groovy
+++ vars/withAWSCli.groovy
@@ -1,16 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name, Closure body) {
-    //sh 'pip3 install awscli --user'
-    //sh 'export PATH=~/.local/bin:$PATH;cmd'
-    //sourceAmiId = sh(script: "${findLatestAmi}", returnStdout: true).trim()
-    //println(name)
-    echo "awscli groovy"
-    //println(this.region)
-    //this.sourceAmiId = "test"
-    //sh 'pip3 install awscli --user'
-    //sh 'find ~/.local/bin -name aws'
-    withEnv(['MYTOOL_HOME=~/.local/bin']) {
-        sh '$MYTOOL_HOME/./aws --version'
-    }
+    println "delegate:" + body.delegate.class.name
+    println "owner: " + body.owner.class.name
 }
