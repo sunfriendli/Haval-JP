d57446422d0faeb5ed12a7ee57b5f17363b46c22
sunfriendli <pengyang_fly@163.com>
2018-08-15 17:21:10 +0800

    commit.sh

--- vars/withAWSCli.groovy
+++ vars/withAWSCli.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String param, boolean needResult, Closure body) {
+def call(Closure body) {
 
-    sh 'pip3 install awscli --user'
-    def command = "PATH=$PATH:$HOME/.local/bin;" + param
-    println command
-    if (needResult) {
-        body.call(command)
-    } else {
-        sh command
-    }
+    // sh 'pip3 install awscli --user'
+    // def command = "PATH=$PATH:$HOME/.local/bin;" + param
+    // println command
+    // if (needResult) {
+    //     body.call(command)
+    // } else {
+    //     sh command
+    // }
 
-    //println test
-    //withEnv(["PATH+AWSCLI=/home/lisi/.local/bin"], body)
+    sh 'pip3 install --user boto3'
+    withEnv(['PATH+AWSCLI=~/.local/bin'], body)
 }
