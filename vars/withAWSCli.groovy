16fa7ccb76bdc14e591b7da42dfb24d689ec42dd
sunfriendli <pengyang_fly@163.com>
2018-08-15 16:22:58 +0800

    commit.sh

--- vars/withAWSCli.groovy
+++ vars/withAWSCli.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String cmd1, Closure body) {
+def call(Closure body) {
     //def cmd = "PATH=$PATH:/home/lisi/.local/bin;" + cmd1
     //println cmd
     //body.call(cmd)
