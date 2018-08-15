ab49affb4750009fc7fbc46ca98d4d8cc6d2cd85
sunfriendli <pengyang_fly@163.com>
2018-08-15 11:25:21 +0800

    commit.sh

--- vars/withAWSCli.groovy
+++ vars/withAWSCli.groovy
@@ -3,4 +3,7 @@
 def call(String name, Closure body) {
     println "delegate:" + body.delegate.class.name
     println "owner: " + body.owner.class.name
+
+    def mapProperties = body.getProperties()
+    println(mapProperties)
 }
