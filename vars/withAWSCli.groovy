954f1f1c832ebe43f4c7bd59c6426d1ba0da69fc
sunfriendli <pengyang_fly@163.com>
2018-08-15 11:27:10 +0800

    commit.sh

--- vars/withAWSCli.groovy
+++ vars/withAWSCli.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(String name, Closure body) {
-    println "delegate:" + body.delegate.class.name
-    println "owner: " + body.owner.class.name
+    //println "delegate:" + body.delegate.class.name
+    //println "owner: " + body.owner.class.name
 
     def mapProperties = body.getProperties()
     println(mapProperties)
