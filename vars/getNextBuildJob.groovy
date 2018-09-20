5866f9459d1b58ac8c51fa3be4eeff947bd781cf
sunfriendli <pengyang_fly@163.com>
2018-09-20 15:11:39 +0800

    Test base.

--- vars/getNextBuildJob.groovy
+++ vars/getNextBuildJob.groovy
@@ -3,7 +3,7 @@
 def call(String fileName, String job, List parents) {
 
     def yaml = readYaml file: fileName
-    parents.each { yaml = yaml[it][children] }
+    parents.each { yaml = yaml[it]['children'] }
     yaml = yaml[job]
     yaml.remove('children')
     println yaml
