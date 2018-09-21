9b13f5714cf54a337bbad4e9e5f5307233a2d74a
sunfriendli <pengyang_fly@163.com>
2018-09-21 10:22:43 +0800

    Test trigger job.

---
+++ vars/triggerJob.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+
+def getBuildParameters(def yaml) {
+    def parameters = []
+    yaml.each {
+        k, v -> 
+        if (k != 'children') {
+            parameters.add("string(name: ${k}, value: ${v}),")
+        }
+    }
+    return parameters
+}
+
+def buildJob(def name, def yaml) {
+    yaml = yaml[name]
+    build(job: name, wait: true, propagate: true, parameters: getBuildParameters(yaml))
+    if (yaml.containkey('children')) {
+        yaml = yaml['children']
+        yaml.each {
+            k, v ->
+                buildJob(k, v) 
+        }
+    }
+}
+
+def call(Map m = [:]) {
+    def root = m.get('root', '')
+    def file = m.get('file', '')
+
+    assert root
+    assert file
+    
+    def yaml = readYaml file: file
+    buildJob(root, yaml)
+}
